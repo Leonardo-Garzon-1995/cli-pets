271e99e0cde888dc7759d60fc1a42f706ab0fdf6
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-11-05 05:47:47 +0100

    first commit

---
+++ bin/cli-pets.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+import { greetOwner, adopt, renderPet } from '../lib/commands.js'
+
+
+program
+  .name('cli-pets')
+  .description('Terminal Pets you can have fun with')
+  .version('1.0.0')
+
+program.command("greet")
+  .description("Greet the owner")
+  .action(greetOwner)
+
+program.command("adopt")
+  .description("Adopt a pet")
+  .action(adopt)
+
+program.command("myPet <species>")
+  .description("renders pet")
+  .action(renderPet)
+
+
+program.parse()
